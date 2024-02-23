7b8ec16be6cdddd91eb0e5e9e92e5294b5e7142a
Máté László Jobbágy <jobbagymatte@gmail.com>
2024-02-23 13:17:01 +0100

    Add project files.

---
+++ UtemezErettsegiFeladat/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UtemezErettsegiFeladat;
+
+namespace Utemez
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var taborok = new List<Taborok>();
+            using var sr = new StreamReader(
+                path: @"..\..\..\bin\Debug\taborok.txt",
+                encoding: Encoding.UTF8);
+            while (!sr.EndOfStream) taborok.Add(new(sr.ReadLine()));
+
+            Console.WriteLine($"2. feladat\nAz adatsorok száma: {taborok.Count}");
+
+            Console.WriteLine("3. feladat:");
+            foreach (var zenei in taborok)
+            {
+                if (zenei.TaborTema == "zenei")
+                {
+                    Console.WriteLine($"Zenei tábor kezdődik {zenei.KezdHo}. hó {zenei.KezdNap}. napján.");
+                }
+            }
+
+            Console.WriteLine("4. feladat");
+            //int legtobbJel = 0;
+            //string legnepszerubb;
+            //foreach (var legtobb in taborok)
+            //{
+            //    if (legtobb.DiakBetujel.Count() > legtobbJel)
+            //    {
+            //        legtobbJel = legtobb.DiakBetujel.Count();
+            //    }
+            //    if (legtobb.DiakBetujel.Count() == legtobbJel)
+            //    {
+                    
+            //    }
+                
+            //}
+
+            //Console.WriteLine("7. feladat");
+
+
+            //string betu;
+            //Console.Write("Adja meg egy tanuló betűjelét: ");
+            //betu = Console.ReadLine();
+            //foreach (var tanulo in taborok)
+            //{
+            //    if (tanulo.DiakBetujel.Contains(betu))
+            //    {
+            //        Console.WriteLine($"{tanulo.TaborTema}");
+            //    }
+            //}
+
+        }
+    }
+}
---
+++ UtemezErettsegiFeladat/Taborok.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtemezErettsegiFeladat
+{
+    internal class Taborok
+    {
+        public int KezdHo { get; set; }
+        public int KezdNap { get; set; }
+        public int VegHo { get; set; }
+        public int VegNap { get; set; }
+        public string DiakBetujel { get; set; }
+        public string TaborTema { get; set; }
+
+        public Taborok(string r)
+        {
+            var v = r.Split('\t');
+            KezdHo = int.Parse(v[0]);
+            KezdNap = int.Parse(v[1]);
+            VegHo = int.Parse(v[2]);
+            VegNap = int.Parse(v[3]);
+            DiakBetujel = v[4];
+            TaborTema = v[5];
+        }
+    }
+}
